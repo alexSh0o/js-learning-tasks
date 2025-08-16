a4e666fc6f411317faa24c2f43cb2ccf00606c75
alexSh0o <alexa.d.tarik@gmail.com>
2025-08-16 15:19:38 +0500

    feat: add solution for task #103

---
+++ strings/103-population-growth.js
@@ -0,0 +1,12 @@
+function yearsToTarget(initialPopulation, naturalIncrease, immigration, targetPopulation) {
+  let years = 0;
+  while (initialPopulation < targetPopulation) {
+    initialPopulation = Math.floor(initialPopulation);
+    initialPopulation =
+      initialPopulation + immigration + (initialPopulation * naturalIncrease) / 100;
+    years++;
+  }
+  return years;
+}
+
+console.log(yearsToTarget(1000, 2, 50, 1214)); // 3
